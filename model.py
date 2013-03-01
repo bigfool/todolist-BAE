61e70d4b1f22a8322114fc90aaaef058225cf37b
Zhenfang Qi <qizhenfang@qq.com>
2013-03-01 11:07:27 +0800

    Create model.py

---
+++ model.py
@@ -0,0 +1,24 @@
+# _*_ coding:utf-8 _*_
+import web
+import web.db
+from bae.core import const
+
+dbname = "fMvMkjlGfBCUrliDeKce"
+
+db = web.database(
+    dbn='mysql',
+    host=const.MYSQL_HOST,
+    port=int(const.MYSQL_PORT),
+    user=const.MYSQL_USER,
+    passwd=const.MYSQL_PASS,
+    db=dbname
+)
+
+def get_todos():
+    return db.select('todo', order='id')
+
+def new_todo(text):
+    return db.insert('todo', titel=text)
+
+def del_todo(id):
+    db.delete('todo', where='id=$id', vars=locas())
